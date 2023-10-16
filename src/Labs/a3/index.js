f502634145cf4799d191a40f081b6176fe4fbeae
Riddhi Gupta <riddhig566@gmail.com>
2023-10-16 15:10:55 -0400

    completed lab

---
+++ src/Labs/a3/index.js
@@ -0,0 +1,21 @@
+import Classes from "./Classes";
+import ConditionalOutput from "./ConditionalOutput";
+import JavaScript from "./JavaScript";
+import PathParameters from "./PathParameters";
+import Styles from "./Styles";
+import TodoList from "./todo/TodoList";
+
+function Assignment3() {
+    return (
+    <div>
+        <h1>Assignment 3</h1>
+        <ConditionalOutput/>
+        <Styles/>
+        <Classes/>
+        <PathParameters/>
+        <JavaScript/>
+        <TodoList/>
+    </div>
+    );
+}
+export default Assignment3;

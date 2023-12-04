1ac2bf9a52613aed2d4263337d8ed6a0e2be73ee
Riddhi Gupta <riddhig566@gmail.com>
2023-12-03 23:38:03 -0500

    fixed padding issue

--- src/App.js
+++ src/App.js
@@ -5,23 +5,19 @@ import HelloWorld from "./Labs/a3/HelloWorld";
 import Kanbas from "./Kanbas";
 import Project from "./Project";
 
-import { ChakraProvider } from '@chakra-ui/react'
-
 function App() {
   return (
-    <ChakraProvider>
-      <HashRouter>
-        <div>
-          <Routes>
-            <Route path="/" element={<Navigate to="/Labs" />} />
-            <Route path="/hello" element={<HelloWorld />} />
-            <Route path="/project/*" element={<Project />} />
-            <Route path="/Labs/*" element={<Labs />} />
-            <Route path="/Kanbas/*" element={<Kanbas />} />
-          </Routes>
-        </div>
-      </HashRouter>
-    </ChakraProvider>
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="/Labs" />} />
+          <Route path="/hello" element={<HelloWorld />} />
+          <Route path="/project/*" element={<Project />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 export default App;
